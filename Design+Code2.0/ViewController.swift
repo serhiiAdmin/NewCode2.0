9717f3442dd1e0bf9b193c1533da064a779dcf35
SerhiiAdmin <pp113eb@gmail.com>
2024-02-29 20:03:59 +0200

    Navigation Controller

---
+++ Design+Code2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Design+Code2.0
+//
+//  Created by SerhiiAdmin on 29.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
